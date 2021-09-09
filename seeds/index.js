37f5154af94911cf9a1c745d4b493a5ad7a5c460
Daisy Sherwood <daisy_sherwood@hotmail.co.uk>
2021-09-09 16:55:32 +0200

    add basic styling

--- seeds/index.js
+++ seeds/index.js
@@ -3,6 +3,7 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers')
 const Campground = require('../models/campground');
 const campground = require('../models/campground');
+const { toLocaleString } = require('./cities');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -18,9 +19,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random()*20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+            price
         })
         await camp.save()
     }
